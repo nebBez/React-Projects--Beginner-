edecf288d287bed49caa12292fdef30100e30bf4
Neb <nebeyo1234@hotmail.com>
2023-07-26 23:07:38 +0800

    clock finished

--- clock_app/src/App.js
+++ clock_app/src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="app"></div>
+
+  // const d = new Date();
+ // const timeRN = (d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+
+
+  const [time, changeTime] = useState(new Date());
+
+
+  useEffect(()=>{
+
+    setInterval(()=>changeTime(new Date()), (1000))
+  }, []
   )
+
+  return (
+
+    <div className="app">
+      <h2>Simple Clock App</h2>
+      <h1>{time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds()}</h1>
+
+    </div>
+
+  )
+
 }
 
 export default App;
