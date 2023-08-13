91e92efdd31ae8e1a992730514aab3adc02169f2
Neb <nebeyo1234@hotmail.com>
2023-08-13 12:39:41 +0800

    .

--- tic_tac_toe_app/src/App.js
+++ tic_tac_toe_app/src/App.js
@@ -6,16 +6,20 @@ import Square from './components/Square.js';
 function Board() {
   
   
-  const [squares, setSquares] = useState(Array(9).fill('n'));
+  const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i){
+    if (squares[i] || calculateWinner(squares)){
+      
+      return;
+    }
     const nextSquares = squares.slice();
-    if (!xIsNext){
-      nextSquares[i] = "o";
+    if (xIsNext){
+      nextSquares[i] = "x";
     }
     else{
-      nextSquares[i] = "x";
+      nextSquares[i] = "o";
     }
     setXIsNext(!xIsNext);
     setSquares(nextSquares);
@@ -23,6 +27,8 @@ function Board() {
 
   return (
     <> {/* <> </> tags can be used instead of div*/}
+      <h1>Tic Tac Toe Game</h1>
+      
       <div className="board-row">
         <Square value={squares[0]} clickHandler={()=>handleClick(0)} />
         <Square value={squares[1]} clickHandler={()=>handleClick(1)}/>
@@ -40,6 +46,26 @@ function Board() {
       </div>
     </>  
     );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;
