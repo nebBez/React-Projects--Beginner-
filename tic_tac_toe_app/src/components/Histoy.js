f6e06970386e6b7bd4c3663ce5e8e106cc7660d0
Neb <nebeyo1234@hotmail.com>
2023-08-15 13:55:22 +0800

    history

---
+++ tic_tac_toe_app/src/components/Histoy.js
@@ -0,0 +1,15 @@
+
+
+
+function History (props){
+    return(
+        <>
+            map function for each element in array
+            <div>
+                <button>Move {props.turn}</button>
+            </div>
+        </>
+    )
+}
+
+export default History;
